d8884ce6d33d56eb009b601f39250af6e8963a39
Varga Bence <wben0802@gmail.com>
2023-07-23 23:55:56 +0200

    add framebuffer class

---
+++ includes/DSAFrameBuffer.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#include <GL/glew.h>
+
+#include <memory>
+
+
+class DSAFrameBuffer {
+public:
+    DSAFrameBuffer() = default;
+
+    explicit DSAFrameBuffer(int num) : ids(std::make_unique<GLuint[]>(num)), num(num) {
+        glCreateFramebuffers(num, ids.get());
+    }
+
+    ~DSAFrameBuffer() {
+        if (ids){
+            glDeleteFramebuffers(num, ids.get());
+        }
+    }
+private:
+    std::unique_ptr<GLuint[]> ids;
+    int num = 0;
+};
